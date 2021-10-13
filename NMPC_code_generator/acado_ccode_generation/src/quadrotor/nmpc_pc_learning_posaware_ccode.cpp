aa748394b04e60f26aa5dc035c427536a0b65d92
Mohit <mohit.mehndiratta@sensible4.fi>
2021-10-13 10:28:17 +0300

    updates code_generator for posaware NMPC.

--- NMPC_code_generator/acado_ccode_generation/src/quadrotor/nmpc_pc_learning_posaware_ccode.cpp
+++ NMPC_code_generator/acado_ccode_generation/src/quadrotor/nmpc_pc_learning_posaware_ccode.cpp
@@ -7,7 +7,6 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <boost/algorithm/string.hpp>
-#include <math.h>
 
 int main()
 {
@@ -41,16 +40,6 @@ int main()
     const double m = 3.8;   // kg
     const double g = 9.81;  // m/s^2
 
-    // equation for s
-    IntermediateState n = pow((pow((x - px) * (x - px), 2) + pow((y - py) * (y - py), 2) + pow((z - pz) * (z - pz), 2)),
-                              0.5);   //norm of vector n
-    IntermediateState n1 = (px - x);  //vector n components n=[n1;n2;n3]
-    IntermediateState n2 = (py - y);
-    IntermediateState n3 = (pz - z);
-    IntermediateState s =
-        (1 / n) * (cos(psi) * cos(theta) - sin(phi) * sin(psi) * sin(theta) * n1 - cos(theta) * sin(psi) +
-                   cos(psi) * sin(phi) * sin(theta) * n2 - cos(phi) * sin(theta) * n3);
-
     // Model equations:
     DifferentialEquation f;
 
@@ -68,10 +57,22 @@ int main()
     f << dot(w) == q_rate * u - p_rate * v - g * cos(phi) * cos(theta) + (1 / m) * (Fz) + Fz_dist;
     //    f << dot(w) == q_rate*u - p_rate*v - g*cos(phi)*cos(theta) + (1/m)*(Fz);
 
+    // equation for s
+    //    IntermediateState n =
+    //        std::pow((pow((x - px) * (x - px), 2) + pow((y - py) * (y - py), 2) + pow((z - pz) * (z - pz), 2)),
+    //                 0.5);                //norm of vector n
+    IntermediateState n1 = (px - x);  //vector n components n=[n1;n2;n3]
+    IntermediateState n2 = (py - y);
+    IntermediateState n3 = (pz - z);
+    IntermediateState norm_n = sqrt(n1 * n1 + n2 * n2 + n3 * n3);
+    IntermediateState s =
+        (1 / norm_n) * (cos(psi) * cos(theta) - sin(phi) * sin(psi) * sin(theta) * n1 - cos(theta) * sin(psi) +
+                        cos(psi) * sin(phi) * sin(theta) * n2 - cos(phi) * sin(theta) * n3);
+
     // Reference functions and weighting matrices:
     Function h, hN;
     h << x << y << z << u << v << w << s << phi << theta << psi << Fz;
-    hN << x << y << z << u << v << w << s;
+    hN << x << y << z << u << v << w;
     //    h << x << y << z << u << v << w << phi << theta << psi<< Fz << Fx_dist << Fy_dist << Fz_dist;
     //    hN << x << y << z << u << v << w << Fx_dist << Fy_dist << Fz_dist;
 
